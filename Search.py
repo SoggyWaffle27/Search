1a2b4d0e7a64a8d639c4e2915b9e15741f4eaab9
Ben Long <155552749+SoggyWaffle27@users.noreply.github.com>
2024-12-03 19:19:05 +0000

    Onya Marx

--- Search.py
+++ Search.py
@@ -11,23 +11,22 @@ def seqSearch(x, list, pos=0):
         pos += 1
     return -1
 
-def halfSearch(x, list):
-    min = 0
+def halfSearch(x, list, min=0):
     max = len(list)
     while True:
-        if len(list[min:max]) == 0:
+        if len(list[min:max]) == 1:
             return -1
-        index = len(list[min:max]) // 2
-        if list[min:max][index] == x:
-            return index + min
-        if index < x:
+        index = (len(list[min:max]) // 2) + min
+        if list[index] == x:
+            return index
+        elif list[index] < x:
             min = index
-        else:
+        elif list[index] > x:
             max = index
 
 print(In(0, [1,2,3]))
 print(In(1, [1,2,3]))
 print(seqSearch(0, [1,2,3]))
-print(seqSearch(2, [1,2,3]))
+print(seqSearch(2, [1,2,3]))  
 print(halfSearch(0, [1,2,3,4]))
-print(halfSearch(14, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]))
+print(halfSearch(17, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]))
